56202aa9ac8f63d1237a4c8789b4e01137a2ee13
ibis-esma <54937568+ibis-esma@users.noreply.github.com>
2019-12-10 20:26:03 +0200

    Add files via upload

---
+++ week8_exp2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+//call by reference
+//pointer dizisi adresleri tutuyor
+
+void func(int *x[]){
+   for(int i = 0; i < 4; ++i){
+      printf("x[%d]'daki adres = %p, o adresteki deger: %d\n", i, (x+i), *(x+i));
+   }
+}
+
+
+
+int main()
+{
+   int x[4]={1,3,5,7};
+
+   func(x);
+
+   return 0;
+}
